08cfff5794da9c86ae55493f0d58253888962147
Charles Carpenter <carpe359@gmail.com>
2022-06-08 18:01:43 -0500

    asdf

--- expressFrame/HostWebSite.js
+++ expressFrame/HostWebSite.js
@@ -2,22 +2,38 @@ const express = require('express');
 const path = require("path");
 const app = express();
 app.listen(8080, () =>
-             console.log("Server running... port: 8080"));
+    console.log("Server running... port: 8080"));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-
-app.get('/', function(request, response){
-   response.sendFile(path.join(__dirname, "", "form.html"));
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, "./", "", "form.html"));
 });
 
-app.post('/',  function(request, response){
 
-    response.redirect("/display.html")
-})
+app.get('/display', function(req, res){
+    res.sendFile(path.join(__dirname, "./", "", "display.html"));
+    
+});
+
+let obj = {}
+
+app.post('/display', function(req, res){
+
+    console.log(req.body);
+    obj = req.body;
+    
+   
+    for (const key in obj) {
+
+        res.send( key + " " + obj[key]);
+        }
+        // res.send(text);
+    })
 
 
-app.get('/display', function(request, response){
-    response.sendFile(path.join(__dirname, "", "display.html"));
- });
+
+
 
 
