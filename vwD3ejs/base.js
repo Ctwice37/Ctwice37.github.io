fe2f4cac6a304e83d3f2423bdcf57e6c77be0e0c
Charles Carpenter <carpe359@gmail.com>
2022-06-08 13:46:51 -0500

    sdf

--- vwD3ejs/base.js
+++ vwD3ejs/base.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 var path = require("path")
 var ejs = require("ejs")
+app.listen(8080, () =>
+             console.log("Server running... port: 8080"));
 
 
-app.set();
-app._router
 
-app.set('view engine', html);
+
+app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
 
 
-app.get
+app.get("/admin", function(req, resp){
+    console.log("Admin Landing")
+    resp.render("myFile", {list: ["apple", "banana", "grape"]});
+})
